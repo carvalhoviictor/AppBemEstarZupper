b0cf1e17bae901a05e84e5cbdc8e358f6607db40
Gabriela Godoy Marques <gabriela.godoy@zup.com.br>
2022-08-08 10:03:49 -0300

    atualização

--- app/src/main/java/br/com/zup/projectfinal/data/PexelsAPI.kt
+++ app/src/main/java/br/com/zup/projectfinal/data/PexelsAPI.kt
@@ -2,11 +2,14 @@ package br.com.zup.projectfinal.data
 
 import br.com.zup.projectfinal.data.model.PexelsResponse
 import retrofit2.http.GET
-
+import retrofit2.http.HEAD
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface PexelsAPI {
-
-    @GET("search?query=frutas&query=verduras")
-    suspend fun getImagesPexels(): PexelsResponse
-
+    @GET("search")
+    suspend fun getImagesPexels(
+        @Header("Authorization") auth: String,
+        @Query("query") query: String
+    ): PexelsResponse
 }
