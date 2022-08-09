7f70faf9d19195e2821593db1bbbe6785d276c55
carvalhoviictor <victor.carvalho@zup.com.br>
2022-08-09 01:30:16 -0300

    ImpAprimoramento grÂáfico nos layouts das telas de Splash e login

--- app/src/main/java/br/com/zup/projectfinal/ui/login/view/LoginFragment.kt
+++ app/src/main/java/br/com/zup/projectfinal/ui/login/view/LoginFragment.kt
@@ -34,7 +34,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvRegisterNow.setOnClickListener {
+        binding.bvRegisterNow.setOnClickListener {
             goToRegister()
         }
 
