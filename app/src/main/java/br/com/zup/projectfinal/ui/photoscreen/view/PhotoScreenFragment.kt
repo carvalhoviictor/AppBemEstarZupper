baadb8ed3863a2acc157ecd9ff540cee18b1cb5c
jennysival <jeniffersival@gmail.com>
2022-08-10 10:18:24 -0300

    Atualizar navigation

--- app/src/main/java/br/com/zup/projectfinal/ui/photoscreen/view/PhotoScreenFragment.kt
+++ app/src/main/java/br/com/zup/projectfinal/ui/photoscreen/view/PhotoScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.zup.projectfinal.databinding.FragmentPhotoScreenBinding
+
 import java.text.SimpleDateFormat
 import java.util.*
 
