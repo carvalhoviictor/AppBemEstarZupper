71f751908c3f1332e50bb45551ddddaf29c41638
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-08-10 14:25:21 -0300

    Funcoes para implementar bottom navigation

--- app/src/main/java/br/com/zup/projectfinal/ui/InitialActivity.kt
+++ app/src/main/java/br/com/zup/projectfinal/ui/InitialActivity.kt
@@ -2,14 +2,45 @@ package br.com.zup.projectfinal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import br.com.zup.projectfinal.R
 import br.com.zup.projectfinal.databinding.ActivityInitialBinding
 
 class InitialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInitialBinding
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInitialBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginFragment -> hideBottomNav()
+                R.id.registerFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavigation.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigation.visibility = View.GONE
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
