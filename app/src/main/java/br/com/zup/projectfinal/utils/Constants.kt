e6d576094c862c3f8d496ed4af85ab53bd5073aa
jennysival <jeniffersival@gmail.com>
2022-08-14 19:35:04 -0300

    Atualização de constantes

--- app/src/main/java/br/com/zup/projectfinal/utils/Constants.kt
+++ app/src/main/java/br/com/zup/projectfinal/utils/Constants.kt
@@ -25,6 +25,7 @@ const val REQUIRED_FIELD = "Campo obrigatório"
 const val NOTE_LIST_ERROR = "Não foi possível carregar a lista"
 const val CHALLENGES_LIST_ERROR = "Não foi possível carregar os desafios"
 const val CONGRATULATION = "Parabéns por mais um desafio concluído!"
+const val CONGRATULATION_LEVEL = "Parabéns! Você subiu de nível!"
 
 const val BENEFIT_MOVIMENTE = "Movi[mente]"
 const val BENEFIT_MOVIMENTE_LOGO = R.drawable.movimente_logo
@@ -38,6 +39,15 @@ const val BENEFIT_GYMPASS_URL = "https://site.gympass.com/br"
 const val BENEFIT_REINTEGRAR = "Reintegrar"
 const val BENEFIT_REINTEGRAR_LOGO = R.drawable.reintegrar_zupnet
 const val BENEFIT_REINTEGRAR_URL = "https://zup1.typeform.com/to/SVDJuH4I?typeform-source=www.google.com"
+const val BENEFIT_BEM_VIVER = "Bem Viver"
+const val BENEFIT_BEM_VIVER_LOGO = R.drawable.bem_viver
+const val BENEFIT_BEM_VIVER_URL = "https://zup1.typeform.com/to/zAUJtW0A?typeform-source=www.google.com"
+const val BENEFIT_GESTAR = "Gestar"
+const val BENEFIT_GESTAR_LOGO = R.drawable.gestar
+const val BENEFIT_GESTAR_URL = "https://zup1.typeform.com/to/yW1fEtRn?typeform-source=www.google.com"
+const val BENEFIT_ATENDIMENTO_COVID = "Atendimento Covid"
+const val BENEFIT_ATENDIMENTO_COVID_LOGO = R.drawable.atendimento_covid
+const val BENEFIT_ATENDIMENTO_COVID_URL = "https://zup1.typeform.com/to/YnDMi82D?typeform-source=www.google.com"
 
 const val TAKEABREAK_NOREUNION = "Período sem reuniões"
 const val TAKEABREAK_NOREUNION_SUBTITLE = "Sugerimos que os times tenham um período sem reuniões para focar em produzir sem interrupções."
@@ -51,3 +61,20 @@ const val TAKEABREAK_TIMEREUNION_LOGO = R.drawable.timereunion
 
 const val ERROR = "Error"
 const val DIVISOR = "------> "
+
+const val PREF_KEY = "SAVED_CHALLENGES"
+const val PREF_DATE_KEY = "CURRENT_DATE"
+const val PREF_CH_ONE_NAME_KEY = "CHALLENGE_ONE"
+const val PREF_CH_TWO_NAME_KEY = "CHALLENGE_TWO"
+const val PREF_CH_THREE_NAME_KEY = "CHALLENGE_THREE"
+const val PREF_CH_FOUR_NAME_KEY = "CHALLENGE_FOUR"
+
+const val PREF_CH_ONE_POINT_KEY = "CHALLENGE_ONE_POINT"
+const val PREF_CH_TWO_POINT_KEY = "CHALLENGE_TWO_POINT"
+const val PREF_CH_THREE_POINT_KEY = "CHALLENGE_THREE_POINT"
+const val PREF_CH_FOUR_POINT_KEY = "CHALLENGE_FOUR_POINT"
+
+const val PREF_CHECKED_ONE = "CHECKONE"
+const val PREF_CHECKED_TWO = "CHECKTWO"
+const val PREF_CHECKED_THREE = "CHECKTREE"
+const val PREF_CHECKED_FOUR = "CHECKFOUR"
